be3941a3c093182a22dd2fe3234645c7c9dbe1b0
mndekwe-dot <m.ndekwe@alustudent.com>
2026-04-03 16:17:22 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,15 +6,12 @@ import requests
 
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
-
     users = requests.get("{}/users".format(base_url)).json()
     all_tasks = {}
-
     for user in users:
         user_id = str(user.get("id"))
         username = user.get("username")
         all_tasks[user_id] = []
-
         todos = requests.get("{}/todos?userId={}".format(base_url, user_id)).json()
         for task in todos:
             all_tasks[user_id].append({
@@ -22,7 +19,6 @@ if __name__ == "__main__":
                 "task": task.get("title"),
                 "completed": task.get("completed")
             })
-
     filename = "todo_all_employees.json"
     with open(filename, mode="w", encoding="utf-8") as jsonfile:
         json.dump(all_tasks, jsonfile)
